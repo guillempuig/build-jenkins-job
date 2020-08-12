ea7c45e4db066a534dea3a53ef656c6ca554baa5
guillem.puig@goldenspear.com <England237892>
2020-08-12 17:02:37 +0200

    experimento num 12315

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -13,8 +13,8 @@ echo "job path $JOB_PATH"
 
 url="$JENKINS_TOKEN@$JENKINS_URL$JOB_PATH/buildWithParameters?token=$JENKINS_TOKEN"
 echo "$url"
-protocol="http://"$JENKINS_USER
-echo protocol
+protocol="http://$JENKINS_USER"
+echo $protocol
 echo "$protocol$url"
 
 output=$(curl -i -X  POST "${url}" | grep Location | tail -1 | sed 's/[^ ]* //')
