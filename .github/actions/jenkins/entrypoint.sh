1b0def7855811ba65e9d77de4df66e9c17a0aa51
guillem.puig@goldenspear.com <England237892>
2020-08-12 12:06:36 +0200

    add user input, add secrets

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -2,6 +2,9 @@
 
 echo "jenkins url $1"
 echo "jenkins token $2"
-echo "job path $3"
+echo "user $3"
+echo "job path $4"
+# shellcheck disable=SC2027
+echo "complete path ""$3"":""$2""@""$1""""$4"" "
 time=$(date)
 echo "::set-output name=time::$time"
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-echo "jenkins url $1"
-echo "jenkins token $2"
-echo "job path $3"
-time=$(date)
-echo "::set-output name=time::$time"
