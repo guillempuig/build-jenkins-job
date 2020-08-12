20ad2a8ae8894f8a8928751e8291897522cf0703
guillem.puig@goldenspear.com <England237892>
2020-08-12 14:40:12 +0200

    unquote

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -10,7 +10,7 @@ echo "jenkins token $2"
 echo "user $3"
 echo "job path $4"
 
-jenkins_url="http://${user}:${token}@${url}${job_path}"
+jenkins_url="http://"${user}":"${token}"@"${url}""${job_path}""
 echo "$jenkins_url"
 jenkins_url="${jenkins_url}/buildWithParameters?token=$2"
 
