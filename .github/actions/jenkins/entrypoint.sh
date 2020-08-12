cb48ab08d19f5d1c24b7e1e54fac14769788d17e
guillem.puig@goldenspear.com <England237892>
2020-08-12 14:45:11 +0200

    apply //$'\r'/

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -10,10 +10,10 @@ echo "jenkins token $2"
 echo "user $3"
 echo "job path $4"
 
-jenkins_url="http://${user}:${token}@${url}${job_path}"
+# shellcheck disable=SC2039
+jenkins_url="http://${user//$'\r'/}:${token//$'\r'/}@${url//$'\r'/}${job_path//$'\r'/}"
 echo "$jenkins_url"
 jenkins_url="${jenkins_url}/buildWithParameters?token=$2"
-
 echo "$jenkins_url"
 
 # output=$(curl -i -X  POST "$jenkins_url" | grep Location | tail -1 | sed 's/[^ ]* //')
