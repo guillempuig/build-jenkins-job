ac3231fa17ef28a15087f968b1384a509f3939c4
guillem.puig@goldenspear.com <England237892>
2020-08-12 12:12:32 +0200

    fix jenkins_url, rename job

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -5,7 +5,7 @@ echo "jenkins token $2"
 echo "user $3"
 echo "job path $4"
 # shellcheck disable=SC2027
-jenkins_url=$("complete path ""$3"":""$2""@""$1""""$4""/buildWithParameters?token=$2")
+jenkins_url="$3:$2@$1$4/buildWithParameters?token=$2"
 echo "$jenkins_url"
 time=$(date)
 echo "::set-output name=time::$time"
