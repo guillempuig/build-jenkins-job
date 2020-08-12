64b3b12f14e664826b5e89ac4535ddd6cb7204a0
guillem.puig@goldenspear.com <England237892>
2020-08-12 14:38:04 +0200

    lightweight to do tests to concatenating

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh -l
 
+url=$1
+token=$2
+user=$3
+job_path=$4
 echo "jenkins url $1"
+echo "jenkins url ${url}"
 echo "jenkins token $2"
 echo "user $3"
 echo "job path $4"
 
-jenkins_url="http://$3:$2@$1$4"
+jenkins_url="http://${user}:${token}@${url}${job_path}"
 echo "$jenkins_url"
 jenkins_url="${jenkins_url}/buildWithParameters?token=$2"
 
 echo "$jenkins_url"
 
-output=$(curl -i -X  POST "$jenkins_url" | grep Location | tail -1 | sed 's/[^ ]* //')
+# output=$(curl -i -X  POST "$jenkins_url" | grep Location | tail -1 | sed 's/[^ ]* //')
 
 echo "This is my output ${output}"
 
