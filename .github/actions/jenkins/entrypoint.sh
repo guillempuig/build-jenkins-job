a1288ef84530daad33a5f81a391222b24dc94590
guillem.puig@goldenspear.com <England237892>
2020-08-13 17:18:19 +0200

    dma skldnmaknfkansifonas

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -5,6 +5,7 @@ JENKINS_URL=$1
 JENKINS_TOKEN=$2
 JENKINS_USER=$3
 JOB_PATH=$4
+QUEUE_TIMEOUT=$5
 
 echo "jenkins url $JENKINS_URL"
 echo "jenkins token $JENKINS_TOKEN"
@@ -17,10 +18,9 @@ protocol="http://"
 protocol+="$url"
 echo "${protocol}"
 
-BRANCH_NAME=""
 
 queue_url=$(curl -i -X  POST "$protocol" \
-   --data "BRANCH_NAME=${BRANCH_NAME}" \
+   --data "QUEUE_TIMEOUT=$QUEUE_TIMEOUT" \
      | grep Location | tail -1 | sed 's/[^ ]* //')
 #
 echo "Queue location: ${queue_url}"
@@ -29,18 +29,31 @@ queue_item=$(echo "${queue_url}" | grep -Po '[\/](\d{4})[\/]')
 queue_number=${queue_item///}
 echo "Job in queue with number: $queue_number"
 
+sleep QUEUE_TIMEOUT
+
 build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number')
+
+if [ "$build_number" == "" ]
+then
+  echo "job still not running"
+fi
+
 
 requests=0
 echo "build_number is: "$build_number
 while [ "$build_number" == "" ] &&  [ $requests -lt 10 ]; do
   sleep 2
+  echo "Job still in queue..."
   build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number')
   requests=$((requests+1))
-  echo "Job still in queue..."
 done
 
 echo "build_number is: "$build_number
+
+if [ "$build_number" == "" ]
+then
+  echo "job not running"
+fi
 
 status=$(curl --silent --output -X GET $JENKINS_URL/job/Fluid/job/fluid-controller-deploy/${build_number}/api/json?petty=true | jq '.result')
 echo $status
