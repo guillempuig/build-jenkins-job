0942ae9bee45e20cf30ebbb6f996c0a26133e9da
guillem.puig@goldenspear.com <England237892>
2020-08-13 17:46:38 +0200

    test

--- .github/actions/jenkins/entrypoint.sh
+++ .github/actions/jenkins/entrypoint.sh
@@ -29,39 +29,38 @@ queue_item=$(echo "${queue_url}" | grep -Po '[\/](\d{4})[\/]')
 queue_number=${queue_item///}
 echo "Job in queue with number: $queue_number"
 
-sleep QUEUE_TIMEOUT
+build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number' )
+echo $build_number
 
-build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number')
-
-if [ "$build_number" == "" ]
-then
-  echo "job still not running"
-fi
-
-
-requests=0
-echo "build_number is: "$build_number
-while [ "$build_number" == "" ] &&  [ $requests -lt 10 ]; do
-  sleep 2
-  echo "Job still in queue..."
-  build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number')
-  requests=$((requests+1))
-done
-
-echo "build_number is: "$build_number
-
-if [ "$build_number" == "" ]
-then
-  echo "job not running"
-fi
-
-status=$(curl --silent --output -X GET $JENKINS_URL/job/Fluid/job/fluid-controller-deploy/${build_number}/api/json?petty=true | jq '.result')
-echo $status
-
-if [ "$status" == "" ]
-then
-  echo "void"
-fi
+#if [ "$build_number" == "" ]
+#then
+#  echo "job still not running"
+#fi
+#
+#
+#requests=0
+#echo "build_number is: "$build_number
+#while [ "$build_number" == "" ] &&  [ $requests -lt 10 ]; do
+#  sleep 2
+#  echo "Job still in queue..."
+#  build_number=$(curl --silent --output -X GET $JENKINS_URL/queue/item/$queue_number/api/json?pretty=true | jq '.executable.number')
+#  requests=$((requests+1))
+#done
+#
+#echo "build_number is: "$build_number
+#
+#if [ "$build_number" == "" ]
+#then
+#  echo "job not running"
+#fi
+#
+#status=$(curl --silent --output -X GET $JENKINS_URL/job/Fluid/job/fluid-controller-deploy/${build_number}/api/json?petty=true | jq '.result')
+#echo $status
+#
+#if [ "$status" == "" ]
+#then
+#  echo "void"
+#fi
 #while [ "$status" == "" ]; do
 #    sleep 1
 #    echo "Job running..."
