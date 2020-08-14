878769dcdb36b1e05831e14f74869b9a2c3d0cf5
guillem.puig@goldenspear.com <England237892>
2020-08-14 10:22:45 +0200

    try jenkins lib

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -2,6 +2,7 @@
 
 import sys
 import requests
+import jenkins
 
 JENKINS_URL = sys.argv[1]
 JENKINS_TOKEN = sys.argv[2]
@@ -13,10 +14,15 @@ print(JENKINS_USER)
 print(JENKINS_TOKEN)
 print(JOB_PATH)
 
+server = jenkins.Jenkins(JENKINS_USER, username=JENKINS_USER, password=JENKINS_TOKEN)
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
 url = "http://"+JENKINS_USER+":"+JENKINS_TOKEN+"@"+JENKINS_URL+JOB_PATH+"/buildWithParameters?token="+JENKINS_TOKEN
 print(url)
 
-x = requests.post(url, data={"QUEUE_TIMEOUT": "2"}).json()
+x = requests.post(url, data={"QUEUE_TIMEOUT": "2"})
 print(x)
 print(x.status_code)
 print(x.json())
