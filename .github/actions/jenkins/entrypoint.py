eb169a56d82ae8672b904520128bf1460c7e1d33
guillem.puig@goldenspear.com <England237892>
2020-08-14 09:38:22 +0200

    change py

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -1,4 +1,13 @@
 import sys
-print(f"This is the name of the script: {sys.argv[0]}")
-print(f"Number of arguments: {len(sys.argv)}")
-print(f"The arguments are:  {str(sys.argv)}")
+
+print("inside script")
+
+
+def greed():
+    print("hello")
+
+
+greed()
+# print(f"This is the name of the script: {sys.argv[0]}")
+# print(f"Number of arguments: {len(sys.argv)}")
+# print(f"The arguments are:  {str(sys.argv)}")
