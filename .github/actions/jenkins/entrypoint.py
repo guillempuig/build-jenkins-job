cffa9b91cfe1c6b912d8ff23b6f17f33e01351a5
guillem.puig@goldenspear.com <England237892>
2020-08-14 09:53:59 +0200

    REQUEST

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import requests
 
 JENKINS_URL = sys.argv[1]
 JENKINS_TOKEN = sys.argv[2]
@@ -10,3 +11,11 @@ print(JENKINS_URL)
 print(JENKINS_USER)
 print(JENKINS_TOKEN)
 print(JOB_PATH)
+
+url = "http://"+JENKINS_USER+":"+JENKINS_TOKEN+"@"+JENKINS_URL+JOB_PATH+"/buildWithParameters?token="+JENKINS_TOKEN
+print(url)
+
+x = requests.post(url, data={"QUEUE_TIMEOUT": "2"})
+
+print(x.text)
+
