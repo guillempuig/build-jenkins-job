f0bb09d60c1e896b804a13f582eab6cf805407f8
guillem.puig@goldenspear.com <England237892>
2020-08-14 09:35:32 +0200

    sys.args

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -1,4 +1,4 @@
 import sys
-print("This is the name of the script: ", sys.argv[0])
-print("Number of arguments: ", len(sys.argv))
-print("The arguments are: ", str(sys.argv))
+print(f"This is the name of the script: {sys.argv[0]}")
+print(f"Number of arguments: {len(sys.argv)}")
+print(f"The arguments are:  {str(sys.argv)}")
