d70da3a321d188bac2bcf7d9161517ca972aa498
guillem.puig@goldenspear.com <England237892>
2020-08-14 14:26:44 +0200

    add always to stop controller

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,5 +5,4 @@ import time
 def test_endpoint():
     time.sleep(30)
     resp = requests.get("http://209.133.199.114:8500/services")
-    print(resp.json())
     assert isinstance(resp.json(), list)
