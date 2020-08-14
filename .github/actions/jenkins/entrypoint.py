0ea1645ae5ea90780d4e119bf61a2343d41608a4
guillem.puig@goldenspear.com <England237892>
2020-08-14 12:53:58 +0200

    USE ENV VARS

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -17,7 +17,7 @@ print(JENKINS_USER)
 print(JENKINS_TOKEN)
 print(JOB_PATH)
 
-server = jenkins.Jenkins("http://209.133.201.194:8080", username=JENKINS_USER, password=JENKINS_TOKEN)
+server = jenkins.Jenkins(f"http://{JENKINS_URL}", username=JENKINS_USER, password=JENKINS_TOKEN)
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
@@ -56,9 +56,6 @@ def get_status(job_name: str, build_number: int) -> str:
 
 
 while not (status := get_status('Fluid/fluid-controller-deploy', build_number)):
-    print("using walrus")
-    print(status)
-    print("============")
     time.sleep(1)
 
 print(status)
