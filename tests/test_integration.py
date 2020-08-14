f5612e15028cf514a9810606b4ce06403bbf3e0e
guillem.puig@goldenspear.com <England237892>
2020-08-14 14:09:52 +0200

    integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,10 @@
+import pytest
+import requests
+import time
+
+
+def test_endpoint():
+    time.sleep(30)
+    resp = requests.get("http://209.133.199.114:8500/services")
+    print(resp.json())
+    assert isinstance(resp.json(), list)
