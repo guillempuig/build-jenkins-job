36b020ea4e6335527555425da2e3955129a74a8b
guillem.puig@goldenspear.com <England237892>
2020-08-14 09:13:30 +0200

    testsssssssssss

--- .github/actions/jenkins/init.sh
+++ .github/actions/jenkins/init.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 ls -l
+
+JENKINS_URL=$1
+JENKINS_TOKEN=$2
+JENKINS_USER=$3
+JOB_PATH=$4
+
+echo "jenkins url $JENKINS_URL"
+echo "jenkins token $JENKINS_TOKEN"
+echo "user $JENKINS_USER"
+echo "job path $JOB_PATH"
+
+cd /
+
+ls -l
