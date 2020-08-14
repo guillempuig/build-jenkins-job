913606282659335674a724967a894b58dc64c9de
guillem.puig@goldenspear.com <England237892>
2020-08-14 11:11:31 +0200

    check key

--- .github/actions/jenkins/entrypoint.py
+++ .github/actions/jenkins/entrypoint.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 import jenkins
+import time
 
 JENKINS_URL = sys.argv[1]
 JENKINS_TOKEN = sys.argv[2]
@@ -19,8 +20,8 @@ user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
 
-jobs = server.get_jobs()
-print(jobs)
+# jobs = server.get_jobs()
+# print(jobs)
 server.build_job('Fluid/fluid-controller-deploy', parameters={"QUEUE_TIMEOUT": "2"}, token=JENKINS_TOKEN)
 queue_info = server.get_queue_info()
 id = queue_info[0].get('id')
@@ -34,7 +35,11 @@ print(url)
 x = requests.get(url)
 print(x)
 print(x.status_code)
-print(x.json())
+
+if "executable" not in x.json():
+    time.sleep(10)
+    x = requests.get(url)
+    print(x.json())
 
 
 # print()
