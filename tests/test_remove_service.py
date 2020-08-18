ad1da93178df64f9446dffab23d0cb86c569d031
guillem.puig@goldenspear.com <England237892>
2020-08-18 17:34:52 +0200

    test remove image

--- tests/test_remove_service.py
+++ tests/test_remove_service.py
@@ -10,7 +10,13 @@ controller = URL + ":" + PORT
 
 
 def test_remove_service():
-    service_name = f"{CLIENT}_{ENV}_testing"
-    fluid_controller = f"{controller}/service/{service_name}"
+    service_name = f"{CLIENT}_{ENV}_testing_image"
+    fluid_controller = f"{controller}/service"
 
+    rm_resp = requests.delete(fluid_controller+f"/{service_name}")
+    assert rm_resp.status_code == 200
 
+    # retrieve list of services and check testing_image has been removed
+    services = requests.get(fluid_controller)
+    assert services.status_code == 200
+    assert len(list(filter(lambda service: service['name'] == 'testing_image', services.json()))) == 0
